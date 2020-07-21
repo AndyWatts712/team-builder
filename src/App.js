fd9765446c8e5ba001b63d7d68e70131aef5d333
megawatts68 <andrew.f.watts@gmail.com>
2020-07-22 03:21:09 +0700

    Basic framework, imports.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TeamList from './components/TeamList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team List</h1>
+      <TeamList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TeamList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TeamList() {
+
+    return(
+        null
+    )
+}
+export default TeamList;
